40c889892a89cbba9e450aa9957366f6756bd026
朱学凯 <xuekaizhu@whu.edu.cn>
2025-10-15 23:40:09 +0800

    update

--- verl_FlowRL/command/training/math/flowrl_7B_math.sh
+++ verl_FlowRL/command/training/math/flowrl_7B_math.sh
@@ -6,13 +6,13 @@ n_gpus_per_node=8
 tensor_model_parallel_size=1
 save_freq=50
 
-dapo_train_path=../data/math_data/dapo-math-17k.parquet
-r1_test_path=../data/math_data/validation.parquet
+dapo_train_path=/mnt/petrelfs/linzhouhan/xuekaizhu/dev/FlowRL/data/math_data/dapo-math-17k.parquet
+r1_test_path=/mnt/petrelfs/linzhouhan/xuekaizhu/dev/FlowRL/data/math_data/validation.parquet
 
 experiment_name="flowrl_qwen_7b_math_source_code"
 max_prompt_length=2048
 max_response_length=8192
-OUTPUT_DIR=../checkpoints/FlowRL/math/7B/$experiment_name
+OUTPUT_DIR=/mnt/petrelfs/linzhouhan/xuekaizhu/dev/FlowRL/checkpoints/FlowRL/math/7B/$experiment_name
 
 set -x
 
